ed0cd57a90523a9e8de45f80aa99e2f60ead0578
shareekaepps <shareekaepps@icloud.com>
2021-10-18 12:21:00 -0400

    some more changes to code needed

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-fetch("https://ghibliapi.herokuapp.com/films")
+let review = document.querySelector("#yreview");
+let film = document.querySelector("#release-date");
+let year = document.querySelector("#description");
+let currMovie = document.querySelector("#titles-select");
 
+fetch("https://ghibliapi.herokuapp.com/films")
     .then((response)=> {
         return response.json();
-    }).then((data) => {
-        let movieTitle = data;
-        for(let movie of movieTitle){
-            console.log(movie.title)
-        }
-
-        console.log(movieTitle);
-    }).catch((err) => {
-        console.log(err);
-    });
-
-    let form = document.querySelector("titles");
-    form.addEventListener("submit", (e) =>{
-        e.preventDefault();
-        let selectedTitle = e.target["titles"].value;
-        if(selectedTitle !== "default" ){
-            fetch("https://ghibliapi.herokuapp.com/films/" + selectedTitle)
-            .then((response) => response.json())
-            .then((data) => {
-                let currentTitleEl = document.querySelector("#display-info");
-
-                currentTitleEl.textContent = 
-
-            });
-        }
-        // console.log(selectedTitle);
     })
+    .then((data) => {
+        // console.log(data);
+    })
+    
+        for(let movie of currMovie) {
+            let option = document.createElement("option");
+            option.setAttribute("el", movie.title);
+            option.textContent = movie.title;
+            currMovie.append(option);
+        }
+        let currSele = "";
+        currMovie.addEventListener("input", () => {
+            for(let movie of currMovie) {
+                if(currMovie.value === movie.title) {
+                    currSele = movie;
+
+                    titlesSelect.textContent = movie.title;
+                    film.textContent = movie.release_date;
+                    year.textContent = movie.description;
+                }
+            }
+        })
+        let form = document.querySelector("#submit")
+        form.addEventListener("click", e => {
+
+            //line 33 event listener not being read -- will continue to work on it
+            e.preventDefault();
+        }) 
+    
