10632018554611886ba6b0952ac91b92393e0a6c
shareekaepps <shareekaepps@icloud.com>
2021-10-18 03:34:30 -0400

    landing page, wokring on js integration

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ fetch("https://ghibliapi.herokuapp.com/films")
     }).catch((err) => {
         console.log(err);
     });
+
+    let form = document.querySelector("titles");
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        let selectedTitle = e.target["titles"].value;
+        if(selectedTitle !== "default" ){
+            fetch("https://ghibliapi.herokuapp.com/films/" + selectedTitle)
+            .then((response) => response.json())
+            .then((data) => {
+                let currentTitleEl = document.querySelector("#display-info");
+
+                currentTitleEl.textContent = 
+
+            });
+        }
+        // console.log(selectedTitle);
+    })
